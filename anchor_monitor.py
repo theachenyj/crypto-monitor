a5f649b08998a5508e630f245d0061cef5e9873c
theachenyj <50615263+theachenyj@users.noreply.github.com>
2021-12-09 08:25:50 +0800

    Add files via upload

--- anchor_monitor.py
+++ anchor_monitor.py
@@ -12,10 +12,12 @@ import math
 # 存储上一次数据变量，计算环比
 LAST_TVL = 0.0
 LAST_PRICE = 0.0
+LAST_APY = 0.0
 
 # 定义数据获取地址
 deposit_url = 'https://api.anchorprotocol.com/api/v1/deposit'
 collaterals_url = 'https://api.anchorprotocol.com/api/v1/collaterals'
+apy_url = 'https://api.anchorprotocol.com/api/v1/market/ust'
 price_url = 'https://api2.binance.com/api/v3/ticker/price?symbol=LUNAUSDT'
 
 # 定义Webhook地址
@@ -31,7 +33,7 @@ def get_data():
     req = requests.get(url=deposit_url, headers=headers)
     if req.status_code == 200:
         data = json.loads(req.text)
-        total_deposits = float(data['total_ust_deposits'])
+        total_deposits = float(data['total_ust_deposits'])/1000000
     else:
         print("请求码为{}, 获取 deposit 数据失败".format(req.status_code))
     
@@ -40,9 +42,18 @@ def get_data():
     
     if req.status_code == 200:
         data = json.loads(req.text)
-        total_collaterals = float(data['total_value'])
+        total_collaterals = float(data['total_value'])/1000000
     else:
         print("请求码为{}, 获取 collaterals 数据失败".format(req.status_code))
+
+    # 获取质押APY
+    req = requests.get(url=apy_url, headers=headers)
+    
+    if req.status_code == 200:
+        data = json.loads(req.text)
+        apy = float(data['deposit_apy'])
+    else:
+        print("请求码为{}, 获取质押收益数据失败".format(req.status_code))
 
     # 获取价格
     req = requests.get(url=price_url, headers=headers)
@@ -53,20 +64,21 @@ def get_data():
     else:
         print("请求码为{}, 获取价格数据失败".format(req.status_code))
 
-    return total_deposits+total_collaterals,price
+    return total_deposits+total_collaterals,apy,price
 
 # 初始化
-LAST_TVL,LAST_PRICE = get_data()
+LAST_TVL,LAST_APY,LAST_PRICE = get_data()
 
 # 每个小时执行
 while True:
     data_time =  datetime.now()
-    tvl,price = get_data()
+    tvl,apy,price = get_data()
     tvl_diff = (tvl - LAST_TVL)/LAST_TVL
     price_diff = (price - LAST_PRICE)/LAST_PRICE
 
     data_time_display = data_time.strftime("%Y-%m-%d %H:%M:%S")
     tvl_display = '{:,}'.format(math.floor(tvl))
+    apy_display = '{:.4%}'.format(apy)
     tvl_diff_display = '{:.4%}'.format(tvl_diff)
     price_diff_display = '{:.4%}'.format(price_diff)
 
@@ -75,7 +87,7 @@ while True:
     LAST_TVL = tvl
     LAST_PRICE = price
 
-    message = f"【（测试）Anchor 监控】\n{data_time_display}\ntvl: {tvl_display}  tvl环比: {tvl_diff_display}\nLuna 价格: {price}  Luna 价格环比: {price_diff_display}"
+    message = f"【（测试）Anchor 监控】\n{data_time_display}\nAPY: {apy_display}\ntvl: {tvl_display}  tvl环比: {tvl_diff_display}\nLuna 价格: {price}  Luna 价格环比: {price_diff_display}"
     payload = {
         "username": "Monitor Cat",
         "content": message
